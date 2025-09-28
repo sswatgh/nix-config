96841e0f4b3232401bc6a1e01324048eb8cc9a0e
sswatgh <mail4ssw@tuta.io>
2025-09-28 15:56:08 +0100

    nix: update configuration

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -2,7 +2,26 @@
 
 {
   programs.git = {
+    enable = true;
     userName = "sswatgh";
     userEmail = "mail4ssw@tuta.io";
+    
+    extraConfig = {
+      credential.helper = 
+        if pkgs.stdenv.isDarwin 
+        then "osxkeychain" 
+        else "cache --timeout=3600";
+    };
+    
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+      type = "cat-file -t";
+      dump = "cat-file -p";
+      nixup = "!git add . && git commit -m 'nix: update configuration' && git push";
+    };
   };
 }
