d74e56f533df8bf79e1297042f73126134c8f590
sswatgh <mail4ssw@tuta.io>
2025-10-03 18:42:54 +0100

    nix: update config

--- home/modules/cli/ssh.nix
+++ home/modules/cli/ssh.nix
@@ -1,13 +1,9 @@
 { lib, platform, pkgs, ... }:
+
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    
-    extraConfig = ''
-      AddKeysToAgent yes
-      ${lib.optionalString platform.isDarwin "UseKeychain yes"}
-    '';
     
     matchBlocks = {
       "github.com" = {
@@ -18,18 +14,14 @@
         controlMaster = "auto";
         controlPath = "~/.ssh/master-%r@%h:%p";
         controlPersist = "10m";
+        addKeysToAgent = "yes";
+        useKeychain = lib.mkIf platform.isDarwin "yes";
       };
     };
   };
 
   home.activation.setupSSH = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "Setting up SSH directory..."
     mkdir -p "$HOME/.ssh"
     chmod 700 "$HOME/.ssh"
-    echo "SSH configuration applied - run 'ssh-add --apple-use-keychain ~/.ssh/id_ed25519' to store key in keychain"
   '';
-  
-  home.packages = with pkgs; [
-    openssh
-  ];
 }
