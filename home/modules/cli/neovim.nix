dffe55fef006f8105ee9aba23064b650e1754c97
sswatgh <mail4ssw@tuta.io>
2025-10-08 21:58:58 +0100

    nix: update config

--- home/modules/cli/neovim.nix
+++ home/modules/cli/neovim.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, platform, ... }:
+{ config, lib, pkgs, platform, ... }:
 
 {
   programs.neovim = {
@@ -13,5 +13,36 @@
       echo "Cloning NvChad (starter)..."
       git clone https://github.com/NvChad/starter ${config.home.homeDirectory}/.config/nvim --depth 1
     fi
+  '';
+
+  xdg.configFile."nvim/lua/custom/plugins.lua".text = ''
+    return {
+      {
+        "ray-x/go.nvim",
+        dependencies = {
+          "ray-x/guihua.lua",
+          "neovim/nvim-lspconfig",
+          "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+          require("go").setup()
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()'
+      }
+    }
+  '';
+
+  xdg.configFile."nvim/lua/custom/mappings.lua".text = ''
+    local M = {}
+    M.general = {
+      n = {
+        ["<leader>gg"] = {"<cmd>GoRun<cr>", "Run Go file"},
+        ["<leader>gt"] = {"<cmd>GoTest<cr>", "Run Go test"},
+        ["<leader>gd"] = {"<cmd>GoDebug<cr>", "Debug Go"},
+      }
+    }
+    return M
   '';
 }
